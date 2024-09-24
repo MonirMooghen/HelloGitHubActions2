5bc5c474b0cab2ad0a13dd7e30c7257918b88df8
MonirMooghen <momo0003@stud.kea.dk>
2024-09-24 14:22:49 +0200

    Initial commit

---
+++ src/main/java/kea/iabr/hellogithubactions2/HelloGitHubActions2Application.java
@@ -0,0 +1,13 @@
+package kea.iabr.hellogithubactions2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitHubActions2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitHubActions2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kea/iabr/hellogithubactions2/HelloGitHubActions2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.iabr.hellogithubactions2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitHubActions2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
